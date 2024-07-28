c418bd88f722fc943af2554188bac34274bf1990
KalashQodeleaf <kbharti@qodeleaf.com>
2024-07-29 00:24:01 +0530

    Build: Get The Post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -21,7 +21,37 @@ const create = async (req, res) => {
   }
 };
 
+//Get the post
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    if (!post.visibility && post.userId != req.session.user.id) {
+      const friend = await Friend.findOne({
+        where: {
+          user1: req.session.user.id,
+          user2: post.userId,
+        },
+      });
+      console.log(friend);
+      if (!friend) {
+        return res
+          .status(403)
+          .json({ message: "You are not authorized to view this post" });
+      }
+    }
 
+    res.json(post);
+  } catch (e) {
+    res.status(500).json({ message: "Something went Wrong" });
+  }
+};
 
 
 export {
